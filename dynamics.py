9abdf80ea1dd4b7e7c2d3f5834730a8af17b8747
shubhamkriitr <shubhamkumar.iitr@gmail.com>
2018-05-25 18:40:37 +0530

    First Commit

---
+++ dynamics.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 25 18:20:58 2018
+
+@author: shubham
+"""
+
+class Manipulator:
+    def __init__(self):
+        pass
+    
+    def feed_DH_table(self):
+        pass
+    
+    def feed_link_specs(self):
+        pass
+    
+    def set_limits(self):
+        pass
+    
+    def set_boundary_points(self):
+        pass
+    
+    def reset_manipulator(self):
+        pass
+    
+    def compute_dynamic_model(self):
+        pass
+    
+    @classmethod
+    def _compute_dynamic_model(self):
+        pass
+    
+    def input_excitation_vector(self):
+        pass
